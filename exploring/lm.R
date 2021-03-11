24ca4e95f5fd038d452b62722060f5dc853a4d4a
Justus Karlsson <karlssonjustus@gmail.com>
2021-03-11 22:46:48 +0100

    Add extension mode - updating big csv file with new week

--- exploring/lm.R
+++ exploring/lm.R
@@ -1,63 +1,50 @@
 
-data = read.csv("../data/2021-02-20_200.csv", sep=";")
+data = read.csv("../data/2021-03-06_4/data.csv", sep=";")
+#data = read.csv("../data/2021-02-20_200.csv", sep=";")
 
 
 val = data$vOdds * data$won
-mean(val[val > 0.0])
 data$val = val
+mean(data$val)
+mean(data$vOdds)
 
 
 favorites = data[data$vOdds < 2.0,]
+mean(favorites$val)
 
 
-data = subset(data, select=c(-Plats., -plats, -result, -won))
-n = dim(data)[1]
-set.seed(12345)
-id = sample(1:n, floor(0.8* n))
-train_set = data[id,] 
-test_set = data[-id,]
-
-
-model = lm(val~., train_set)
-
-
-# Always pick yes on a horse with really low odds (a lot of people have betted on it)
-choose_favorite = function(data, rate) {
-  n = dim(data)[1]
-  ans = rep(0, n)
-  for (i in 1:n) {
-    if (data$vOdds[i] <= rate) {
-      ans[i] = 1
-    }
+print_horses = function(horses){
+  for (i in 1:dim(horses)[1]) {
+    s = sprintf("%d-%d", horses$raceIdx[i], horses$horseNumber[i]+1)
+    print(s)
   }
-  return (ans)
 }
 
-# Evaluates how good a model is at deciding whether a horse is going to
-# win or not. More than 1.0 means that it's better than always choosing
-# that it will lose.
-eval = function (pred) {
-  n = dim(test_set)[1]
-  correct = sum(pred == test_set$won) / n
-  lose_rate = sum(1 - test_set$won) / n
-  return (correct - lose_rate + 1)
+v_val = function(){
+  data = subset(data, select=c(-Plats., -plats, -result, -won))
+  n = dim(data)[1]
+  set.seed(12345)
+  id = sample(1:n, floor(0.8* n))
+  train_set = data[id,] 
+  test_set = data[-id,]
   
+  model = lm(val~., train_set)
+  
+  preds = predict(model, newdata=test_set)
+  print(sort(preds, decreasing=TRUE)[1:200])
+  
+  # This really seems like the sweet stop, tested 3 times
+  bets = intersect(which(preds > 1.04), which(preds < 1.2))
+  horses = test_set[bets,]
+  length(horses)
+  print(mean(horses$val))
+  print(mean(horses$vOdds))
+  
+
+  
+  return (horses)
 }
 
-eval_favorite = function(rate) {
-  acc = eval(choose_favorite(test_set, rate))
-  s = sprintf("Only choose horse which every %.1f gambler thinks is going to win (all others horses lose). Eval: %.4f", rate, acc)
-  print(s)
-}
 
 
 
-s = sprintf("Our model. Eval: %.2f", eval(predict(model, newdata=test_set)))
-print(s)
-
-
-preds = predict(model, newdata=test_set)
-sort(preds, decreasing=TRUE)[1:20]
-
-bets = which(preds > 1.0 && preds < 1.2)
-mean(test_set$val[bets])
--- scraper/scrape.py
+++ scraper/scrape.py
@@ -11,55 +11,89 @@ from collections import defaultdict
 import sys
 import pathlib
 import traceback
+import json
 
 from scrape_week import scrape_week
 from Column import factors 
 
 
+ONE_WEEK = timedelta(days=7)
+
+def mkdir(path):
+    if not os.path.isdir(path):
+        os.mkdir(path)
 
 def extend_entries(main, new):
     for key, sequence in new.items():
         main[key] += sequence
 
-def write_to_file(columns, file_name):
+def csv_to_dict(path: str):
+    factor_ids = {}
+    with open(f"{path}/factors.json") as f:
+        d = json.load(f)
+        for key, val in d.items():
+            factor_ids[key] = val
+    data = defaultdict(list)
+    with open(f"{path}/data.csv") as f:
+        column = {i: key.rstrip() for i, key in enumerate(next(f).split(";"))}
+        for line in f:
+            entries = line.split(";")
+            for i, entry in enumerate(entries):
+                data[column[i]].append(entry.rstrip())
+    return data, factor_ids
+
+def write_to_file(columns, file_name, factors_ids):
     headers = list(columns.keys())
     root = pathlib.Path(__file__).parent.parent.absolute()
     dir_path = f"{root}/data"
-    if not os.path.isdir(dir_path):
-        os.mkdir(dir_path)
+    mkdir(dir_path)
+    dir_path = f"{dir_path}/{file_name}"
+    mkdir(dir_path)
 
-    file_path = f"{dir_path}/{file_name}"
-    print("Writing to file: '{}'".format(file_path))
-    with open(file_path, "w") as out:
+    print("Writing to directory: '{}'".format(dir_path))
+    csv_path = f"{dir_path}/data.csv"
+    with open(csv_path, "w") as out:
         out.write(";".join(headers) + "\n")
         size = len(columns[headers[0]])
         for i in range(size):
             vals = []
             for header in headers:
+                if len(columns[header]) <= i:
+                    print(f"Too few for {columns[header]}, {header}, {i}")
                 vals.append(columns[header][i])
             out.write(";".join(vals) + "\n")
+    with open(f"{dir_path}/factors.json", "w") as f:
+        f.write(json.dumps(factors_ids, indent="\t"))
 
 
+def str_to_date(date_str):
+    return datetime(*[int(x) for x in date_str.split("-")])
+
+def date_to_str(date):
+    return date.strftime("%Y-%m-%d")
+
+def parse(browser, weeks, start_date_str, extension):
+    start_date = str_to_date(start_date_str)
 
 
-
-
-
-def parse(browser, weeks: int, start_date_str):
-    start_date = datetime(*[int(x) for x in start_date_str.split("-")])
-    one_week = timedelta(days=7)
-
+    factor_ids = defaultdict(dict)
     columns = defaultdict(list)
+    
+
+
     for i in range(weeks):
-        date = start_date-i*one_week
-        date_str = date.strftime("%Y-%m-%d")
+        date = start_date-i*ONE_WEEK
+        date_str = date_to_str(date)
         columns_i = scrape_week(browser, date_str, i == 0)
         if len(columns_i) > 0:
             print(f"{i+1}/{weeks} done.")
             extend_entries(columns, columns_i)
         else:
             print(f"Parsing of date {date_str} failed...")
-    factor_ids = defaultdict(dict)
+
+    if extension:
+        old_columns, factor_ids = csv_to_dict(extension)
+    
     for header, values in columns.items():
         if header not in factors:
             continue
@@ -67,26 +101,52 @@ def parse(browser, weeks: int, start_date_str):
             if val not in factor_ids[header]:
                 factor_ids[header][val] = len(factor_ids[header])
             columns[header][i] = str(factor_ids[header][val])
+    
+    if extension:
+        extend_entries(old_columns, columns)
+        columns = old_columns
+        print(len(columns["horseAge"]))
+
+        # So we get the proper name
+        weeks += int(extension.split("_")[-1])
 
     if len(columns) == 0:
         print("Failed to parse any week :(")
     else:
-        write_to_file(columns, f"{start_date_str}_{weeks}.csv")
+        write_to_file(columns, f"{start_date_str}_{weeks}", factor_ids)
 
 browser = None
 try:
     arg_parser = ArgumentParser()
 
-    arg_parser.add_argument("--start-date", default="2021-02-20")
+    arg_parser.add_argument("--start-date", default="2021-03-06")
     arg_parser.add_argument("--weeks", default=10, type=int)
+    arg_parser.add_argument("--extension", default="")
+    arg_parser.add_argument("--data-path", default="data")
     
     args = arg_parser.parse_args()
 
+    if args.extension:
+        start_date = args.extension.split("_")[0]
+        cur_date = str_to_date(start_date)
+        # V75 slut kl 19:00
+        cur_date += timedelta(hours=19)
+        weeks = 0
+
+        while cur_date + ONE_WEEK < datetime.now():
+            weeks += 1
+            cur_date += ONE_WEEK
+
+        args.weeks = weeks
+        args.extension = f"{args.data_path}/{args.extension}"
+        args.start_date = date_to_str(cur_date)
+
+    print(args)
     chrome_options = Options()
     browser = webdriver.Chrome(options=chrome_options)
 
     print("Starting parsing")
-    parse(browser, args.weeks, args.start_date)
+    parse(browser, args.weeks, args.start_date, args.extension)
 
 except KeyboardInterrupt:
     print("Scraper cancelled by user")
